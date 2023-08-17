fefa7c5a6fdd557d714f26e8ee55b029a6636b38
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-17 13:30:32 +0800

    refactor

--- src/components/Appointment/__tests__/Form.test.js
+++ src/components/Appointment/__tests__/Form.test.js
@@ -8,10 +8,11 @@ describe("Form", () => {
   const interviewers = [
     {
       id: 1,
-      student: "Sylvia Palmer",
+      name: "Sylvia Palmer",
       avatar: "https://i.imgur.com/LpaY82x.png"
     }
   ];
+
 
   it("renders without student name if not provided", () => {
     const { getByPlaceholderText } = render(<Form interviewers={interviewers} />);
@@ -25,6 +26,7 @@ describe("Form", () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
+
   it("validates that the student name is not blank", () => {
     const onSave = jest.fn();
     // the student prop should be blank or undefined
@@ -36,6 +38,7 @@ describe("Form", () => {
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
+
 
   it("validates that the interviewer cannot be null", () => {
     const onSave = jest.fn();
@@ -49,7 +52,8 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name and interviewer is defined", () => {
+
+  xit("calls onSave function when the name and interviewer is defined", () => {
     const onSave = jest.fn();
 
     const { queryByText } = render(
@@ -58,9 +62,75 @@ describe("Form", () => {
 
     fireEvent.click(queryByText("Save"));
 
+    // do not show error message afer we click "Save" with valid input
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
     expect(queryByText(/please select an interviewer/i)).toBeNull();
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
+
+
+  xit("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave}
+        interviewer={interviewers[0].id} />);
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    // trigger onChange event for the input field
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
+
+
+  // covers code that the 2 previous tests do
+  it("can successfully save after trying to submit an empty student name", () => {
+    const onSave = jest.fn();
+
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave}
+        interviewer={interviewers[0].id} />);
+
+    fireEvent.click(getByText("Save"));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    // error message should now be removed
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByText("Save"));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
+
+
+  it("calls onCancel and resets the input field", () => {
+    const onSave = jest.fn();
+    const onCancel = jest.fn();
+
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} onCancel={onCancel}
+        name="Lydia Mill-Jones" />);
+
+    fireEvent.click(getByText("Save"));
+
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
